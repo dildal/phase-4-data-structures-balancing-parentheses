b42bfc90394a7393c5bc58b2971d7ff46ab0437a
Michael Dally <mddally@gmail.com>
2022-04-11 13:49:40 -0400

    commit tree ba9b850ade315dc1c3b2da61b0bb96fceadd0a8c

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,21 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses(str)
+    paran_stack = Stack.new
+    extra_right = 0
+    str.chars.each do |char| 
+        if char == '('
+            paran_stack.push('(')
+        elsif char == ')'
+            if paran_stack.size == 0
+                extra_right += 1
+            else
+                paran_stack.pop
+            end
+        end
+    end
+
+    paran_stack.size + extra_right
+end
